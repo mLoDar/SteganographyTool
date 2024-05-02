45005c28ff4163b9bc2eae0a2ee03891131ddd6c
mLoDar <mlodarcoding@gmail.com>
2024-05-02 13:07:08 +0200

    Reduced menu selection Removed the documentation section as it will be replaced by a readme file.

--- Program.cs
+++ Program.cs
@@ -56,7 +56,7 @@ namespace SteganographyTool
                     break;
 
                 case ConsoleKey.DownArrow:
-                    selectedOption = (selectedOption + 1 <= 3) ? selectedOption + 1 : selectedOption;
+                    selectedOption = (selectedOption + 1 <= 2) ? selectedOption + 1 : selectedOption;
                     goto LabelDrawUi;
 
                 case ConsoleKey.UpArrow:
@@ -97,8 +97,6 @@ namespace SteganographyTool
             Console.WriteLine("             {0} {1}                  ", selectedOption == 1 ? "[\x1B[94m>\x1B[97m]" : "[ ]", selectedOption == 1 ? $"{options[0].ToUpper()}" : $"{options[0]}");
             Console.WriteLine("             {0} {1}                  ", selectedOption == 2 ? "[\x1B[94m>\x1B[97m]" : "[ ]", selectedOption == 2 ? $"{options[1].ToUpper()}" : $"{options[1]}");
             Console.WriteLine("                                                   ");
-            Console.WriteLine("             {0} {1}                  ", selectedOption == 3 ? "[\x1B[94m>\x1B[97m]" : "[ ]", selectedOption == 3 ? $"{options[2].ToUpper()}" : $"{options[2]}");
-            Console.WriteLine("                                                   ");
             Console.WriteLine("                                                   ");
             Console.WriteLine("                                                   ");
             Console.WriteLine("             (Use the arrow keys and ENTER to navigate)");
@@ -114,10 +112,6 @@ namespace SteganographyTool
 
                 case 2:
                     ReadInformation.Entry();
-                    break;
-
-                case 3:
-                    ShowDocumentation.Entry();
                     break;
 
                 default:
